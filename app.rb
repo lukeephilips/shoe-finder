45d8b7c0abc96a443626344a66103dfbb5d12a22
Luke <luke.e.philips@gmail.com>
2016-12-16 11:41:52 -0800

    create function for removing a shoe from a specific store only

--- app.rb
+++ app.rb
@@ -8,40 +8,57 @@ get ('/') do
   erb(:index)
 end
 
+
+post('/stores/new') do
+  if Store.create(:name => params['store_name'])
+  redirect ('/')
+  end
+end
+
 get ('/stores/:id') do
   @store = Store.find(params['id'])
-  erb(:store)
+  @shoes = Shoe.all
+  erb(:index)
 end
 
 patch ('/stores/:id') do
   @store = Store.find(params['id'])
     if @store.update(:name => params['edit_name'])
       redirect "/stores/#{@store.id}"
-    else
-      erb(:error)
     end
 end
   delete ('/stores/:id') do
     @store = Store.find(params['id'])
       if @store.destroy
         redirect "/"
-      else
-        erb(:error)
       end
   end
-
-post('/stores/new') do
-  if Store.create(:name => params['store_name'])
-  redirect ('/')
-  else
-    erb(:error)
-  end
-end
 
 post('/shoes/new') do
   if Shoe.create(:name => params['shoe_name'])
   redirect ('/')
-  else
-    erb(:error)
+  end
+end
+
+delete('/shoes/:id') do
+  shoe = Shoe.find(params['id'])
+  if shoe.destroy
+  redirect ('/')
+  end
+end
+
+patch ('/stores/:id/shoes') do
+  @store = Store.find(params['id'])
+    shoe_ids = params.fetch('shoe_ids')
+  if @store.update({:shoe_ids => shoe_ids})
+    redirect "/stores/#{@store.id}"
+  end
+end
+
+delete ('/stores/:id/shoes') do
+  @store = Store.find(params['id'])
+    shoe = Shoe.find(params.fetch('shoe_id').to_i)
+  if @store.shoes.delete(shoe)
+    redirect "/stores/#{@store.id}"
   end
 end
