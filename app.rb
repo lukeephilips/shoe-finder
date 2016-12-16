7c300f1d36b556bf7783d726935be58998220ab6
Luke <luke.e.philips@gmail.com>
2016-12-16 09:16:01 -0800

    add forms to front end, complete ability to add stores

--- app.rb
+++ app.rb
@@ -1,3 +1,24 @@
 require('bundler/setup')
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file}
+
+get ('/') do
+  @stores = Store.all
+  erb(:index)
+end
+
+post('/stores/new') do
+  if Store.create(:name => params['store_name'])
+  redirect ('/')
+  else
+    erb(:error)
+  end
+end
+
+post('/shoes/new') do
+  if Shoe.create(:name => params['store_name'], :store_ids => [params['store_id']])
+  redirect ('/')
+  else
+    erb(:error)
+  end
+end
