56c4bf6ac72816e56f3b0884c6f91164fe76d1dd
Luke <luke.e.philips@gmail.com>
2016-12-16 08:36:24 -0800

    pass unit testing for Store#shoes

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,1 +1,17 @@
 require('spec_helper')
+
+describe(Store) do
+  before do
+    @shoe = Shoe.create(:name => 'Nike')
+    @shoe2 = Shoe.create(:name => 'Adidas')
+    @store = Store.create(:name => 'Kmart')
+    @store = Store.create(:name => 'Target')
+  end
+
+  describe('#shoes') do
+    it('returns the shoes that belong to a store') do
+      @store.update(:shoe_ids => [@shoe.id, @shoe2.id])
+      expect(@store.shoes).to(eq([@shoe, @shoe2]))
+    end
+  end
+end
